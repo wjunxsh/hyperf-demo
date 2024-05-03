7dc8345bcee88dd914a0b8f42b1332f2e6ba228e
wjunxsh <1053249119@qq.com>
2024-05-03 10:48:54 +0800

    fff

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -21,7 +21,7 @@ class IndexController extends AbstractController
 
         return [
             'method' => $method,
-            'message' => "He2l22lo12 {$user}.",
+            'message' => "He2lsss22lo12 {$user}.",
         ];
     }
 }
